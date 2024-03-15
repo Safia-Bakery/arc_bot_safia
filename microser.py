c36f6c11e6abb89b06d65548a76bb9d247215c31
Gayratbek <gayratbekakhmedov@gmail.com>
2024-03-15 10:09:54 +0500

    edit dat

--- microser.py
+++ microser.py
@@ -22,12 +22,12 @@ def get_db():
     return db
 
 
-
 def transform_list(lst, size, key):
     if key=='id':
+    
         return [[f"{item.id}" for item in lst[i:i+size]] for i in range(0, len(lst), size)]
     if key=='name':
-        return [[f"{item.parentfillial.name}" for item in lst[i:i+size]] for i in range(0, len(lst), size)]
+        return [[f"{item.name}" for item in lst[i:i+size]] for i in range(0, len(lst), size)]
 
 
 def generate_text(lst):
