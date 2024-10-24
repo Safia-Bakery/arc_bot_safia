8854dffcb8f456fe8eed2ace65b247886ded303b
Gayratbek <gayratbekakhmedov@gmail.com>
2024-10-24 16:50:58 +0500

    add message to group

--- ratings.py
+++ ratings.py
@@ -17,7 +17,9 @@ from microser import transform_list
 
 import datetime
 import calendar
+import pytz
 
+time_zone = pytz.timezone('Asia/Tashkent')
 
 
 async def input_rating(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -30,9 +32,15 @@ async def input_rating(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
         else:
             get_user_id = crud.get_user_tel_id(id=update.message.from_user.id)
 
-            crud.add_general_comment(user_id=get_user_id.id,comment=entered_data)
+            rating_created= crud.add_general_comment(user_id=get_user_id.id,comment=entered_data)
+
             await update.message.reply_text(f"Главное меню",
                                             reply_markup=ReplyKeyboardMarkup(bot.manu_buttons, resize_keyboard=True))
+            #send message to another group by chat id
+            current_formatted_date =  datetime.datetime.now(time_zone).strftime("%d.%m.%Y %H:%M")
+            text_to_send = f"💬 Новый отзыв #{rating_created.id}s\nАвтор: {get_user_id.full_name}\nНомер: {get_user_id.phone_number}\nОтзыв: {entered_data}\nДата: {current_formatted_date}\n\n✍️Комментарии:  {entered_data}"
+
+            await context.bot.send_message(chat_id='-1002223465896', text=text_to_send)
             return bot.MANU
 
     else:
