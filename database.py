2eb1e7491fa139cb4e88444bd185afbe35b41e3e
Data Engineer <dataeng27@gmail.com>
2024-11-06 19:08:47 +0500

    added new features for it

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost:5432/arc"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:2726@localhost:5432/arc"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
