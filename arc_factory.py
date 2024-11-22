7199fe540b7741fa7a922a2cbe87d07cf1c2f2cf
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-22 23:20:05 +0500

    sd

--- arc_factory.py
+++ arc_factory.py
@@ -26,26 +26,24 @@ async  def arc_factory_managers(update: Update, context: ContextTypes.DEFAULT_TY
                                         reply_markup=ReplyKeyboardMarkup(bot.manu_buttons, resize_keyboard=True))
         return bot.MANU
 
-    # try:
-    get_manager_divisions = crud.get_arc_factory_managers(name=entered_data)
-    if get_manager_divisions:
+    try:
+        get_manager_divisions = crud.get_arc_factory_managers(name=entered_data)
+        if get_manager_divisions:
 
 
-        divisions = crud.get_manager_divisions(manager_id=get_manager_divisions[0].id)
-        reply_keyboard = bot.transform_list(divisions, 2, 'name')
+            divisions = crud.get_manager_divisions(manager_id=get_manager_divisions[0].id)
+            reply_keyboard = bot.transform_list(divisions, 2, 'name')
+            reply_keyboard.append(['⬅️ Назад'])
+            context.user_data['manager'] = get_manager_divisions[0].id
+            await update.message.reply_text('Выберите отдел',reply_markup=ReplyKeyboardMarkup(reply_keyboard,resize_keyboard=True))
+            return bot.ARCFACTORYDIVISIONS
+
+    except:
+        managers = crud.get_arc_factory_managers()
+        reply_keyboard = bot.transform_list(managers, 2, 'name')
         reply_keyboard.append(['⬅️ Назад'])
-        context.user_data['manager'] = get_manager_divisions[0].id
-        await update.message.reply_text('Выберите отдел',reply_markup=ReplyKeyboardMarkup(reply_keyboard,resize_keyboard=True))
-        print('returning')
-        return bot.ARCFACTORYDIVISIONS
-    print('is not returing')
-
-    # except:
-    #     managers = crud.get_arc_factory_managers()
-    #     reply_keyboard = bot.transform_list(managers, 2, 'name')
-    #     reply_keyboard.append(['⬅️ Назад'])
-    #     await update.message.reply_text('Выберите менеджера',reply_markup=ReplyKeyboardMarkup(reply_keyboard,resize_keyboard=True))
-    #     return bot.ARCFACTORYMANAGER
+        await update.message.reply_text('Выберите менеджера',reply_markup=ReplyKeyboardMarkup(reply_keyboard,resize_keyboard=True))
+        return bot.ARCFACTORYMANAGER
 
 
 
@@ -58,34 +56,22 @@ async def arc_factory_divisions(update: Update, context: ContextTypes.DEFAULT_TY
         await update.message.reply_text('Выберите менеджера',reply_markup=ReplyKeyboardMarkup(reply_keyboard,resize_keyboard=True))
         return bot.ARCFACTORYMANAGER
 
-    # try:
-    categories = crud.get_category_list(department=1,sphere_status=2)
-    reply_keyboard = bot.transform_list(categories, 2, 'name')
-    reply_keyboard.append(['⬅️ Назад'])
-    # current_divisions = crud.get_manager_divisions(context.user_data['manager'])
+    try:
+        categories = crud.get_category_list(department=1,sphere_status=2)
+        reply_keyboard = bot.transform_list(categories, 2, 'name')
+        reply_keyboard.append(['⬅️ Назад'])
 
-    # for i in current_divisions:
-    #     await update.message.reply_text(i.name)
-    #
-    #     print(i.name)
-    #     if str(i.name).strip() == str(entered_data):
-    #         print()
-    #         context.user_data['division_id'] = i.id
-    #         await update.message.reply_text('found')
-    #         break
-    #
-    # print(context.user_data['division_id'])
-    current_division = crud.get_manager_division_by_name(name=entered_data,manager_id=context.user_data['manager'])
-    context.user_data['division_id'] = current_division.id
-    await update.message.reply_text(f"Пожалуйста выберите категорию:",
-                                    reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True))
-    return bot.CATEGORY
+        current_division = crud.get_manager_division_by_name(name=entered_data,manager_id=context.user_data['manager'])
+        context.user_data['division_id'] = current_division.id
+        await update.message.reply_text(f"Пожалуйста выберите категорию:",
+                                        reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True))
+        return bot.CATEGORY
 
-    # except:
-    #     divisions = crud.get_manager_divisions(manager_id=context.user_data['manager'])
-    #     reply_keyboard = bot.transform_list(divisions, 2, 'name')
-    #     reply_keyboard.append(['⬅️ Назад'])
-    #     await update.message.reply_text('Выберите отдел',reply_markup=ReplyKeyboardMarkup(reply_keyboard,resize_keyboard=True))
-    #     return bot.ARCFACTORYDIVISIONS
+    except:
+        divisions = crud.get_manager_divisions(manager_id=context.user_data['manager'])
+        reply_keyboard = bot.transform_list(divisions, 2, 'name')
+        reply_keyboard.append(['⬅️ Назад'])
+        await update.message.reply_text('Выберите отдел',reply_markup=ReplyKeyboardMarkup(reply_keyboard,resize_keyboard=True))
+        return bot.ARCFACTORYDIVISIONS
 
 
