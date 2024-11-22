f71ac083fc05b680c742b581899632cf7f6b4f7e
Gayratbek <gayratbekakhmedov@gmail.com>
2024-11-22 22:53:32 +0500

    sd

--- arc_factory.py
+++ arc_factory.py
@@ -63,7 +63,7 @@ async def arc_factory_divisions(update: Update, context: ContextTypes.DEFAULT_TY
     current_divisions = crud.get_manager_divisions(context.user_data['manager'])
     for i in current_divisions:
         await update.message.reply_text(i.name)
-        if str(i.name).strip() == entered_data:
+        if str(i.name).strip() == str(entered_data):
             context.user_data['division_id'] = i.id
             await update.message.reply_text('found')
             break
